94fca4c7dbfd1d4717f177217c03f7fce1dd697f
Gonie <111123554+GonieAnima@users.noreply.github.com>
2025-03-26 11:29:00 +0100

    Cambiado cosas de el programa

--- programa/main_nano1.ino
+++ programa/main_nano1.ino
@@ -1,18 +1,12 @@
-//          DEBOUNCER DE BOTÓN (SI ES POSIBLE CON UN LM 555)
-//          LED ENGANCHABLE CON RESISTENCIA ENGANCHADA (CON DISEÑO 3D DE SUJETADOR DE BOTÓN)
-//          BOTÓN ALARGABLE
-
 //BIBLIOTECAS
 #include <Servo.h>
 
 //VARIABLES DE CALCULO Y AUXILIARES
 int val=0;
 int dump;
-int velocidad = 0;        // NO TENEMOS NI PUTA IDEA, HACER PRUEBAS
 
 //VARIABLES DE TIEMPO
 int errorT = 30;            //Temporizador de margen de error para botones
-int blinkT = 500;           //Temporizador de parpadeo
 int debounceT = 200;        //Temporizador de debouncing
 int raveT = 30;             //Temporizador de rave mientras se mantiene el boton pulsado RAVE RAVE Tsk Tsk Tsk ...
 int startT=3000;            //Tiempo de inicio como dictaminado en las reglas
@@ -23,27 +17,15 @@ int giro180;                 //Tiempo para girar 180º
 int pinBut = 10;            //Pin
 int butState = 0;           //Estado del botón
 
-//Lectura de sensores
-void sensor(){
-int SD=digitalRead(pinSD);
-int SI=digitalRead(pinSI);
-}
-
 //INDICADORES
 int pinLed = 13;
 
 //INTERRUPT     (COMUNICACIÓN CON EL SEGUNDO)
-int interruptPin=2;
-
-//Detección de sensores
-int SD=digitalRead(pinSD);
-int SI=digitalRead(pinSI);
-int Detector=0    //Placeholder, cuando sepamos la salida de los sensores lo cambiamos
+int interruptPin=2;           //Pin de interrupción
 
 //PINS DE SENSORES
 int pinSD = 11;                // PIN Sensor Derecho
 int pinSI = 3;                 // PIN Sensor Izquierdo
-
 
 //SETUP DE VARIABLES DE ACTUADORES
 int pinMOIA = 8;                 // Motor Output Izquierdo A
@@ -64,11 +46,7 @@ void setup(){
   pinMode(pinMOIB, OUTPUT);  
   pinMode(pinMODA, OUTPUT); 
   pinMode(pinMODB, OUTPUT); 
-  pinMode(pinSD, INPUT);     //configura los pines como entradas
-  pinMode(pinSI, INPUT); 
-  //SETUP DE ENTRADAS Y SALIDAS PARA ENCENDIDO
-  pinMode(pinBut,INPUT);
-  pinMode(pinLed,OUTPUT);
+
   delay(3000)             //Delay normativo de inicio
 }
 
@@ -123,19 +101,4 @@ void DS(){                  //Rueda Derecha paro
 void IS(){                  //Rueda Izquierda paro
   digitalWrite(pinMOIB,LOW);
   digitalWrite(pinMOIA,LOW);
-}
-
-// FUNCIONES DEL LED 
-void blink(){                    //PARPADEO
-  digitalWrite(pinLed,HIGH);        //Enciende led
-  delay(blinkT);                    //Espera
-  digitalWrite(pinLed,LOW);         //Apaga led
-  delay(blinkT);                    //Espera
-}
-
-void blink_rave(){                //PARPADEO RAPIDO
-  digitalWrite(pinLed,HIGH);        //Enciende led
-  delay(raveT+errorT);              //Espera
-  digitalWrite(pinLed,LOW);         //Apaga led
-  delay(raveT);                     //Espera
 }
